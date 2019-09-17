8af05ea538c5dcf2d0aaf932b25e8432865d3a18
Qualthalas <Vinczee99@gmail.com>
2019-09-17 09:22:43 +0200

    asd

--- ts/content.ts
+++ ts/content.ts
@@ -1,6 +1,7 @@
 ﻿import * as fs from "fs";
 import * as http from "http";
-import * as path from "path";
+import * as url from "url";
+import { ParsedUrlQuery } from "querystring";
 
 export default class Content {
 
@@ -16,15 +17,19 @@ export default class Content {
         res.write("<!DOCTYPE html>");
         res.write("<html lang='hu'>");
         //  fejrész
-        res.write("<head>");
-        res.write("<title>TéglalapTk</title>");
-        res.write("</head>");
+        res.write("<head><title>TéglalapTk</title></head>");
         res.write("<body>");
-
         res.write("<h1>Téglalap területe és kerülete</h1>");
+
+        const query: ParsedUrlQuery = url.parse(req.url, true).query;
         const a: number = 5;
-        const b: number = 5;
+        const b: number = 6;
         res.write(`<h3>a=${a}</h3>`);
+        res.write(`<h3>b=${b}</h3>`);
+        const terulet: number = a * b;
+        const kerulet: number = 2 * (a * b);
+        res.write(`<h2>T=${terulet}</h2`);
+        res.write(`<h2>T=${kerulet}</h2`);
 
         res.write("</body>");
         res.write("</html>");
